4a588e8b70059bcf380010e167ba583b68eb5bb8
ging-dev <gingdz11012001@gmail.com>
2019-11-04 10:19:24 +0700

    Return a message when the search failed

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ API nghe nhạc cho Chatfuel 🎵🎵
 - get nhạc zingmp3
 - nhaccuatui
 - sử dụng cho chatfuel
+# Cách sử dụng
+- `domain/zing-webhook.php?baihat={tenbaihat}` (Server zingmp3) ngoài ra có thể sử dụng webhook.php (Server NCT)
+- kết quả tìm kiếm bài hát có không chính xác như mong muốn
+# Update
+- hiển thị thông báo nếu không tìm được bài hát
+
--- webhook.php
+++ webhook.php
@@ -19,23 +19,25 @@
    
    // Lấy url bài hát đầu tiên  
    
-   preg_match('#key="(.+?)"#', $curl->response, $idbaihat);
-   
+   preg_match('#key="(.+?)"><img class="thumb"#', $curl->response, $idbaihat);
+      
    $curl->reset();
-    
    
-   $curl->get('https://graph.nhaccuatui.com/v1/songs/' . $idbaihat[1] . '?access_token=683501bbad17313976cb2cbe4305fb3d');
+   $chatfuel = new Chatfuel\Chatfuel(TRUE);
    
-   $data = json_decode($curl->response, true);
-   
-   // Phản hồi chatfuel
-   
-   $chatfuel = new Chatfuel\Chatfuel(TRUE);        
-   
-   $text = 'Bài Hát ' . $data['data']['2'] . ' của ' . $data['data']['3'] . ' phải không?';
-   
-   $chatfuel->sendText($text);
-   $chatfuel->sendImage($data['data']['8']); 
-   $chatfuel->sendAudio($data['data']['11']);    
-   
+   if ($idbaihat[1]) {
+       $curl->get('https://graph.nhaccuatui.com/v1/songs/' . $idbaihat[1] . '?access_token=683501bbad17313976cb2cbe4305fb3d');
+       
+       $data = json_decode($curl->response, true);
+       
+       // Phản hồi chatfuel        
+       
+       $text = 'Bài Hát ' . $data['data']['2'] . ' của ' . $data['data']['3'] . ' phải không?';
+       
+       $chatfuel->sendText($text);
+       $chatfuel->sendImage($data['data']['8']); 
+       $chatfuel->sendAudio($data['data']['11']);
+   } else {
+       $chatfuel->sendText('Không tìm được bài hát.');
+   }
 ?>
